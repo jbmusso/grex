c0389f1799b682c32f7e19d990106fc93b03627f
Frank Panetta <frank.panetta@entrendipity.com.au>
2013-05-28 23:05:07 +1000

    added indexing example file

---
+++ examples/grexIndexing.js
@@ -0,0 +1,37 @@
+var g = require('grex');
+var y = "bob";
+g.addVertex(0, {name:y});
+g.commit()
+.then(function(result){
+    if (result) {
+        if (result.success == false) {
+            console.log("Failed to add vertex for", y);
+        } else {
+            console.log("Added a vertex successfully for", y);
+            // Not working yet, no 'grex' method to process this
+            g.createIndex("actor", "Vertex.class").get()
+            .then(function(result){
+                if (result) {
+                    if (result.success == false) {
+                        console.log("Failed to create index  - actor");
+                    } else {
+                        g.idx('actor').put('name', y, g.v(0)).get()
+                        .then(function(result){
+                            if (result) {
+                                if (result.success == false) {
+                                    console.log("Failed to add an index for", y);
+                                } else {
+                                    console.log("Index added successfully for", y);
+                                }
+                            }
+                        }, function(err) {
+                            console.log(err)
+                        });
+                    }
+                }
+            });
+        }
+    }
+}, function(err) {
+    console.log(err)
+});
