b6faa40d58808660c37a68a8af0a227aeaeb4c0c
Frank Panetta <frank.panetta@entrendipity.com.au>
2013-07-24 10:47:26 +1000

    index.js is main entry

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 ;(function(){
-	var orientdb = require(__dirname + '/src/orientdb-js.js');
-	var compat = require(__dirname + '/src/node-compat.js');
+	var grex = require(__dirname + '/src/grex.js');
+	//var compat = require(__dirname + '/src/node-compat.js');
 	/*Override browser specific functions*/
-	orientdb.REST.then = compat.post();
-	orientdb.REST.authenticate = compat.auth();
-	module.exports = orientdb;
+	// orientdb.REST.then = compat.post();
+	// orientdb.REST.authenticate = compat.auth();
+	module.exports = grex;
 })();
