9c7ff44f4cf48557d58fe6d57fc2895fd0a3bf79
Gulthor <jbmusso+github@gmail.com>
2013-10-10 14:16:24 +0200

    Add more property methods

--- src/element.js
+++ src/element.js
@@ -4,15 +4,42 @@ module.exports = Element = (function() {
         this._obj = obj;
     }
 
+    Element.prototype.setProperty = function (k, v) {
+        this._obj[k] = v;
+        return this;
+    };
+
+    Element.prototype.setProperties = function (properties) {
+        for (var key in properties) {
+            this.setProperty(key, properties[key]);
+        }
+        return this;
+    };
+
+    /*
+     * Titan specific method
+     *
+     * Use this instead of setProperty() when setting the value of an indexed
+     * property.
+     */
     Element.prototype.addProperty = function(k, v) {
         this._obj[k] = v;
         return this;
     };
 
-    Element.prototype.setProperty = function (k, v) {
-        this._obj[k] = v;
+    /*
+     * Titan specific method
+     *
+     * Use this instead of setProperties() when setting the values of indexed
+     * properties.
+     */
+    Element.prototype.addProperties = function (properties) {
+        for (var key in properties) {
+            this.addProperty(key, properties[key]);
+        }
         return this;
     };
+
 
     return Element;
 
