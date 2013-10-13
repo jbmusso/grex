0ba33686f02d391c58f2a9e8662003863f3370b4
Gulthor <jbmusso+github@gmail.com>
2013-10-13 17:19:07 +0200

    Extract TransactionCommitter class from Transaction, added several inner methods

--- src/transaction.js
+++ src/transaction.js
@@ -1,117 +1,18 @@
-var q = require("q"),
-    http = require("http"),
-    request = require("request"),
-    Utils = require("./utils"),
-    Element = require("./element"),
-    ActionHandler = require("./actionhandler");
-
-var typeHash = {
-    'integer': 'i',
-    'long': 'l',
-    'float': 'f',
-    'double': 'd',
-    'string': 's',
-    'boolean': 'b',
-    'i': 'i',
-    'l': 'l',
-    'f': 'f',
-    'd': 'd',
-    's': 's',
-    'b': 'b',
-    'list': 'list',
-    'map': 'map'
-};
-
-var isObject = Utils.isObject;
-var isArray = Utils.isArray;
-
-var push = Array.prototype.push;
+var Element = require("./element"),
+    ActionHandler = require("./actionhandler"),
+    TransactionCommitter = require("./transactioncommitter"),
+    addTypes = require("./addtypes");
 
 
 module.exports = (function () {
-    function Trxn(options, typeMap) {
+    function Transaction(options, typeMap) {
+        console.log(typeMap);
+
+        this.committer = new TransactionCommitter(this);
         this.OPTS = options;
         this.typeMap = typeMap;
-        this.transactionArray = [];
+        this.pendingElements = [];
         this.pendingVertices = [];
-    }
-
-    function addTypes(obj, typeDef, embedded, list){
-        console.log("Adding types");
-
-        var tempObj = {};
-        var tempStr = '';
-        var obj2, idx = 0;
-
-        for(var k in obj){
-            if(obj.hasOwnProperty(k)){
-                if(typeDef){
-                    if ((k in typeDef) && isObject(typeDef[k])) {
-                        if(embedded){
-                            if (list) {
-                                obj2 = obj[k];
-
-                                for(var k2 in obj2){
-                                    if(obj2.hasOwnProperty(k2)){
-                                        if(typeDef[k] && (k2 in typeDef[k])){
-                                            tempStr += '(map,(' + addTypes(obj[k], typeDef[k], true) + '))';
-                                        }
-                                    }
-                                }
-                            } else {
-                                tempStr += k + '=(map,(' + addTypes(obj[k], typeDef[k], true) + '))';
-                            }
-                            tempStr = tempStr.replace(')(', '),(');
-                        } else {
-                            tempObj[k] = '(map,(' + addTypes(obj[k], typeDef[k], true) + '))';
-                        }
-                    } else if ((k in typeDef) && isArray(typeDef[k])) {
-                        if(embedded){
-                            tempStr += '(list,(' + addTypes(obj[k], typeDef[k], true, true) + '))';
-                            tempStr = tempStr.replace(')(', '),(');
-                        } else {
-                            tempObj[k] = '(list,(' + addTypes(obj[k], typeDef[k], true, true) + '))';
-                        }
-                    } else {
-                        if(embedded){
-                            if (list) {
-                                if (k in typeDef) {
-                                    idx = k;
-                                    tempStr += '(' + typeHash[typeDef[idx]] + ',' + obj[k] + ')';
-                                } else {
-                                    idx = typeDef.length - 1;
-                                    if (isObject(typeDef[idx])) {
-                                        tempStr += ',(map,(' + addTypes(obj[k], typeDef[idx], true) + '))';
-                                    } else if (isArray(typeDef[idx])){
-                                        tempStr += ',(list,(' + addTypes(obj[k], typeDef[idx], true, true) + '))';
-                                    } else {
-                                      tempStr += '(' + typeHash[typeDef[idx]] + ',' + obj[k] + ')';
-                                    }
-                                }
-                                tempStr = tempStr.replace(')(', '),(');
-                            } else {
-                                if (k in typeDef) {
-                                    tempStr += k + '=(' + typeHash[typeDef[k]] + ',' + obj[k] + ')';
-                                    tempStr = tempStr.replace(')(', '),(');
-                                } else {
-                                    tempObj[k] = obj[k];
-                                }
-                            }
-                        } else {
-                            if (k in typeDef) {
-                                tempObj[k] = '(' + typeHash[typeDef[k]] + ',' + obj[k] + ')';
-                            } else {
-                                tempObj[k] = obj[k];
-                            }
-                        }
-                    }
-                } else {
-                    tempObj[k] = obj[k];
-                }
-            }
-        }
-
-        return embedded ? tempStr : tempObj;
     }
 
 
@@ -126,228 +27,28 @@ module.exports = (function () {
 
             if (actionhandler.addToTransaction) {
                 element._action = action;
-                this.transactionArray.push(addTypes(element, this.typeMap));
+                this.pendingElements.push(addTypes(element, this.typeMap));
             }
 
             return element;
         };
     }
 
-    //returns an error Object
-    function rollbackVertices(){
-        var self = this;
-        var errObj = { success: false, message : "" };
-        //In Error because couldn't create new Vertices. Therefore,
-        //roll back all other transactions
-        console.error('problem with Transaction');
-        self.transactionArray.length = 0; // "clears" array
-
-        for (var i = self.pendingVertices.length - 1; i >= 0; i--) {
-            //check if any vertices were created and create a Transaction
-            //to delete them from the database
-            if('_id' in self.pendingVertices[i]){
-                self.removeVertex(self.pendingVertices[i]._id);
-            }
-        }
-
-        //This indicates that nothing was able to be created as there
-        //is no need to create a tranasction to delete the any vertices as there
-        //were no new vertices successfully created as part of this Transaction
-        self.pendingVertices.length = 0;
-
-        if (!self.transactionArray.length){
-            return q.fcall(function () {
-                errObj.message = "Could not complete transaction. Transaction has been rolled back.";
-
-                return errObj;
-            });
-        }
-
-        //There were some vertices created which now need to be deleted from
-        //the database. On success throw error to indicate transaction was
-        //unsuccessful. On fail throw error to indicate that transaction was
-        //unsuccessful and that the new vertices created were unable to be removed
-        //from the database and need to be handled manually.
-        return postData.call(self, '/tp/batch/tx', { tx: self.transactionArray })
-            .then(function(success){
-                errObj.message = "Could not complete transaction. Transaction has been rolled back.";
-
-                return errObj;
-            }, function(fail){
-                errObj.message = "Could not complete transaction. Unable to roll back newly created vertices.";
-                errObj.ids = self.transactionArray.map(function(item){
-                    return item._id;
-                });
-                self.transactionArray.length = 0;
-
-                return errObj;
-            });
-    }
-
-
-    Trxn.prototype.commit = function post() {
-        var self = this;
-
-        function doCommit() {
-            var promises = [];
-            var transactionElement;
-
-            if(!!self.pendingVertices.length){
-                // We have new vertices to create first!
-
-                for (var i = 0; i < self.pendingVertices.length; i++) {
-                    console.log("With Rexster specific header");
-
-                    promises.push(postData.call(self, '/vertices', addTypes(self.pendingVertices[i], self.typeMap), {'Content-Type':'application/vnd.rexster-typed-v1+json'}));
-                }
-
-                return q.all(promises).then(function(result){
-                    var inError = false;
-                    //Update the _id for the created Vertices
-                    for (var j = 0; j < result.length; j++) {
-                        if('results' in result[j] && '_id' in result[j].results){
-                            self.pendingVertices[j]._id = result[j].results._id;
-                        } else {
-                            inError = true;
-                        }
-                    }
-
-                    if(inError){
-                        return rollbackVertices.call(self)
-                            .then(function(result){
-                                throw result;
-                            },function(error){
-                                throw error;
-                            });
-                    }
-
-                    //Update any edges that may have referenced the newly created Vertices
-                    for (var k = 0; k < self.transactionArray.length; k++) {
-                        transactionElement = self.transactionArray[k];
-
-                        if(transactionElement._type == 'edge' && transactionElement._action == 'create'){
-                            // Replace references to Vertex object by references to Vertex _id.
-                            // TODO: Try replacing the following two checks with getters for _inV and _outV in Edge prototype.
-                            if (isObject(transactionElement._inV)) {
-                                transactionElement._inV = transactionElement._inV._id;
-                            }
-
-                            if (isObject(transactionElement._outV)) {
-                                transactionElement._outV = transactionElement._outV._id;
-                            }
-                        }
-                    }
-
-                    return postData.call(self, '/tp/batch/tx', { tx: self.transactionArray });
-
-                }, function(err){
-                    console.error(err);
-                });
-
-            } else {
-                // We don't have new vertices to create, only edges...
-                for (var k = 0; k < self.transactionArray.length; k++) {
-                    transactionElement = self.transactionArray[k];
-
-                    if(transactionElement._type == 'edge' && transactionElement._action == 'create'){
-                        if (isObject(transactionElement._inV)) {
-                            transactionElement._inV = transactionElement._inV._id;
-                        }
-
-                        if (isObject(transactionElement._outV)) {
-                            transactionElement._outV = transactionElement._outV._id;
-                        }
-                    }
-                }
-
-                return postData.call(self, '/tp/batch/tx', { tx: self.transactionArray });
-            }
-        }
-
-        return doCommit();
+    Transaction.prototype.commit = function() {
+        return this.committer.doCommit();
     };
 
+    Transaction.prototype.addVertex = cud('create', 'vertex');
 
-    function postData(urlPath, data, headers){
-        var self = this;
-        var deferred = q.defer();
+    Transaction.prototype.addEdge = cud('create', 'edge');
 
-        var url = 'http://' + this.OPTS.host + ':' + this.OPTS.port + '/graphs/' + this.OPTS.graph + urlPath;
+    Transaction.prototype.removeVertex = cud('delete', 'vertex');
 
-        var options = {
-            url: url,
-            body: JSON.stringify(data),
-            headers: {
-                'Content-Type': 'application/json' // May be superseded
-            }
-        };
+    Transaction.prototype.removeEdge = cud('delete', 'edge');
 
-        // Specify custom headers, if any
-        if(headers){
-            for(var prop in headers){
-                if(headers.hasOwnProperty(prop)){
-                    options.headers[prop] = headers[prop];
-                }
-            }
-        }
+    Transaction.prototype.updateVertex = cud('update', 'vertex');
 
-        request.post(options, function(err, res, body) {
-            if (err) {
-                // Handle any HTTP request error, not Rexster errors
-                console.error('problem with request: ' + err);
-                return deferred.reject(err);
-            }
+    Transaction.prototype.updateEdge = cud('update', 'edge');
 
-            body = JSON.parse(body);
-
-            if('success' in body && body.success === false){
-                //send error info with reject
-                if(self.pendingVertices && !!self.pendingVertices.length){
-                    //This indicates that all new Vertices were created but failed to
-                    //complete the rest of the tranasction so the new Vertices need deleted
-                    rollbackVertices.call(self)
-                        .then(function(result){
-                            deferred.reject(result);
-                        },function(error){
-                            deferred.reject(error);
-                        });
-                } else {
-                    deferred.reject(body);
-                }
-            } else {
-                //This occurs after pendingVertices have been created
-                //and passed in to postData
-                if(!('results' in body) && self.pendingVertices && !!self.pendingVertices.length){
-                    body.pendingVertices = [];
-                    push.apply(body.pendingVertices, self.pendingVertices);
-                    self.pendingVertices.length = 0;
-                }
-
-                if('tx' in data){
-                    data.tx.length = 0;
-                }
-
-                deferred.resolve(body);
-            }
-
-            return deferred.resolve(body);
-        });
-
-        return deferred.promise;
-    }
-
-
-    Trxn.prototype.addVertex = cud('create', 'vertex');
-
-    Trxn.prototype.addEdge = cud('create', 'edge');
-
-    Trxn.prototype.removeVertex = cud('delete', 'vertex');
-
-    Trxn.prototype.removeEdge = cud('delete', 'edge');
-
-    Trxn.prototype.updateVertex = cud('update', 'vertex');
-
-    Trxn.prototype.updateEdge = cud('update', 'edge');
-
-    return Trxn;
+    return Transaction;
 })();
---
+++ src/transactioncommitter.js
@@ -0,0 +1,267 @@
+var q = require("q"),
+    request = require("request");
+
+var Utils = require("./utils"),
+    isObject = Utils.isObject,
+    addTypes = require("./addtypes");
+
+
+module.exports = TransactionCommitter = (function() {
+
+    function TransactionCommitter(transaction) {
+        this.transaction = transaction;
+    }
+
+
+    TransactionCommitter.prototype.updateEdges = function() {
+        var transactionElement;
+
+        for (var k = 0; k < this.transaction.pendingElements.length; k++) {
+            transactionElement = this.transaction.pendingElements[k];
+
+            if(transactionElement._type == 'edge' && transactionElement._action == 'create'){
+                // Replace references to Vertex object by references to Vertex _id.
+                // TODO: Try replacing the following two checks with getters for _inV and _outV in Edge prototype.
+                if (isObject(transactionElement._inV)) {
+                    transactionElement._inV = transactionElement._inV._id;
+                }
+
+                if (isObject(transactionElement._outV)) {
+                    transactionElement._outV = transactionElement._outV._id;
+                }
+            }
+        }
+    };
+
+
+    TransactionCommitter.prototype.updateVertices = function(result) {
+        console.log("==updateVertices==");
+
+        var inError = false;
+
+        //Update the _id for the created Vertices
+        for (var j = 0; j < result.length; j++) {
+            if('results' in result[j] && '_id' in result[j].results){
+                this.transaction.pendingVertices[j]._id = result[j].results._id;
+            } else {
+                inError = true;
+            }
+        }
+
+        return inError;
+    };
+
+
+    TransactionCommitter.prototype.getPostVerticesPromises = function() {
+        console.log("==getPostVerticesPromises==");
+
+        var promises = [],
+            types,
+            header = {'Content-Type':'application/vnd.rexster-typed-v1+json'};
+
+        for (var i = 0; i < this.transaction.pendingVertices.length; i++) {
+            types = addTypes(this.transaction.pendingVertices[i], this.transaction.typeMap);
+            promises.push(this.postData('/vertices', types, header));
+        }
+
+        return q.all(promises);
+    };
+
+
+    TransactionCommitter.prototype.commitVertices = function() {
+        console.log("==commitVertices==");
+
+        var self = this;
+
+        // return this.getPostVerticesPromises().then(function(result){
+        return this.getPostVerticesPromises().then(function(result){
+            inError = self.updateVertices(result);
+
+            if(inError){
+                return self.rollbackVertices()
+                    .then(function(result){
+                        throw result;
+                    },function(error){
+                        throw error;
+                    });
+            }
+
+            //Update any edges that may have referenced the newly created Vertices
+            self.updateEdges();
+
+            return self.postData('/tp/batch/tx', { tx: self.transaction.pendingElements });
+
+        }, function(err){
+            console.error(err);
+        });
+    };
+
+
+    TransactionCommitter.prototype.commitEdges = function() {
+        console.log("==commitEdges==");
+
+        var transactionElement;
+        // We don't have new vertices to create, only edges...
+        for (var k = 0; k < this.transaction.pendingElements.length; k++) {
+            transactionElement = this.transaction.pendingElements[k];
+
+            if(transactionElement._type == 'edge' && transactionElement._action == 'create'){
+                if (isObject(transactionElement._inV)) {
+                    transactionElement._inV = transactionElement._inV._id;
+                }
+
+                if (isObject(transactionElement._outV)) {
+                    transactionElement._outV = transactionElement._outV._id;
+                }
+            }
+        }
+
+        return this.postData('/tp/batch/tx', { tx: this.transaction.pendingElements });
+    };
+
+
+    TransactionCommitter.prototype.doCommit = function() {
+        console.log("==commit==");
+
+        if(!!this.transaction.pendingVertices.length){
+            // We have new vertices to create first!
+            commit = this.commitVertices();
+        } else {
+            commit = this.commitEdges();
+        }
+
+        return commit;
+    };
+
+
+    TransactionCommitter.prototype.postData = function(urlPath, data, headers) {
+        console.log("==postData==", data, headers);
+
+        var self = this;
+        var deferred = q.defer();
+
+        var url = 'http://' + this.transaction.OPTS.host + ':' + this.transaction.OPTS.port + '/graphs/' + this.transaction.OPTS.graph + urlPath;
+
+        var options = {
+            url: url,
+            body: JSON.stringify(data),
+            headers: {
+                'Content-Type': 'application/json' // May be overridden
+            }
+        };
+
+        // Specify custom headers, if any
+        if(headers){
+            for(var prop in headers){
+                if(headers.hasOwnProperty(prop)){
+                    options.headers[prop] = headers[prop];
+                }
+            }
+        }
+
+        request.post(options, function(err, res, body) {
+            if (err) {
+                // Handle any HTTP request error, not Rexster errors
+                console.error('Problem with request: ' + err);
+                deferred.reject(err);
+            }
+
+            body = JSON.parse(body);
+
+            if('success' in body && body.success === false){
+                //send error info with reject
+                if(self.transaction.pendingVertices && !!self.transaction.pendingVertices.length){
+                    //This indicates that all new Vertices were created but failed to
+                    //complete the rest of the tranasction so the new Vertices need deleted
+                    self.rollbackVertices()
+                    .then(function(result){
+                        deferred.reject(result);
+                    },function(error){
+                        deferred.reject(error);
+                    });
+
+                } else {
+                    deferred.reject(body);
+                }
+            } else {
+                //This occurs after pendingVertices have been created
+                //and passed in to postData
+                if(!('results' in body) && self.transaction.pendingVertices && !!self.transaction.pendingVertices.length){
+                    body.pendingVertices = [];
+                    // push.apply(body.pendingVertices, self.transaction.pendingVertices);
+                    body.pendingVertices.push(self.transaction.pendingVertices);
+
+                    self.transaction.pendingVertices.length = 0;
+                }
+
+                if('tx' in data){
+                    data.tx.length = 0;
+                }
+
+                deferred.resolve(body);
+            }
+        });
+
+        return deferred.promise;
+    };
+
+
+    TransactionCommitter.prototype.rollbackVertices = function() {
+    //returns an error Object
+    // function rollbackVertices(){
+        var self = this;
+        var errObj = {
+            success: false,
+            message : ""
+        };
+        //In Error because couldn't create new Vertices. Therefore,
+        //roll back all other transactions
+        console.error('problem with Transaction');
+        self.transaction.pendingElements.length = 0; // "clears" array
+
+        for (var i = self.transaction.pendingVertices.length - 1; i >= 0; i--) {
+            //check if any vertices were created and create a Transaction
+            //to delete them from the database
+            if('_id' in self.transaction.pendingVertices[i]){
+                self.transaction.removeVertex(self.transaction.pendingVertices[i]._id);
+            }
+        }
+
+        //This indicates that nothing was able to be created as there
+        //is no need to create a tranasction to delete the any vertices as there
+        //were no new vertices successfully created as part of this Transaction
+        self.transaction.pendingVertices.length = 0;
+
+        if (!self.transaction.pendingElements.length){
+            return q.fcall(function () {
+                errObj.message = "Could not complete transaction. Transaction has been rolled back.";
+
+                return errObj;
+            });
+        }
+
+        //There were some vertices created which now need to be deleted from
+        //the database. On success throw error to indicate transaction was
+        //unsuccessful. On fail throw error to indicate that transaction was
+        //unsuccessful and that the new vertices created were unable to be removed
+        //from the database and need to be handled manually.
+        return this.postData('/tp/batch/tx', { tx: self.transaction.pendingElements })
+            .then(function(success){
+                errObj.message = "Could not complete transaction. Transaction has been rolled back.";
+
+                return errObj;
+            }, function(fail){
+                errObj.message = "Could not complete transaction. Unable to roll back newly created vertices.";
+                errObj.ids = self.transaction.pendingElements.map(function(item){
+                    return item._id;
+                });
+
+                self.transaction.pendingElements.length = 0;
+
+                return errObj;
+            });
+    };
+
+    return TransactionCommitter;
+
+})();
