e9827938d9b17ee4811ca574651886553f78b7ef
Gulthor <jbmusso+github@gmail.com>
2014-01-06 17:21:18 +0100

    Switch Transaction to 2 spaces tabs. Sorry git blame.

--- src/transaction/transaction.js
+++ src/transaction/transaction.js
@@ -1,57 +1,57 @@
 var ElementFactory = require("../elementfactory"),
-    ActionHandlerFactory = require("./actionhandlers/actionhandlerfactory"),
-    TransactionCommitter = require("./transactioncommitter");
+  ActionHandlerFactory = require("./actionhandlers/actionhandlerfactory"),
+  TransactionCommitter = require("./transactioncommitter");
 
 
 module.exports = (function () {
-    function Transaction(options, typeMap) {
-        this.committer = new TransactionCommitter(this);
-        this.options = options;
-        this.typeMap = typeMap;
-        this.txArray = [];
-        this.pendingVertices = [];
-    }
+  function Transaction(options, typeMap) {
+    this.committer = new TransactionCommitter(this);
+    this.options = options;
+    this.typeMap = typeMap;
+    this.txArray = [];
+    this.pendingVertices = [];
+  }
 
-    Transaction.prototype.commit = function() {
-        return this.committer.doCommit();
-    };
+  Transaction.prototype.commit = function() {
+    return this.committer.doCommit();
+  };
 
-    /**
-     * Handle an action for an element of given type.
-     *
-     * @param {String} action
-     * @param {String} type
-     * @param {Array} args
-     */
-    Transaction.prototype.handleAction = function(action, type, args) {
-        var element = ElementFactory.build(type);
-        var actionhandler = ActionHandlerFactory.build(element, this, args);
-        return actionhandler.handleAction(action);
-    };
+  /**
+   * Handle an action for an element of given type.
+   *
+   * @param {String} action
+   * @param {String} type
+   * @param {Array} args
+   */
+  Transaction.prototype.handleAction = function(action, type, args) {
+    var element = ElementFactory.build(type);
+    var actionhandler = ActionHandlerFactory.build(element, this, args);
+    return actionhandler.handleAction(action);
+  };
 
-    Transaction.prototype.addVertex = function() {
-        return this.handleAction('create', 'vertex', arguments);
-    };
+  Transaction.prototype.addVertex = function() {
+    return this.handleAction('create', 'vertex', arguments);
+  };
 
-    Transaction.prototype.addEdge = function() {
-        return this.handleAction('create', 'edge', arguments);
-    };
+  Transaction.prototype.addEdge = function() {
+    return this.handleAction('create', 'edge', arguments);
+  };
 
-    Transaction.prototype.removeVertex = function() {
-        return this.handleAction('delete', 'vertex', arguments);
-    };
+  Transaction.prototype.removeVertex = function() {
+    return this.handleAction('delete', 'vertex', arguments);
+  };
 
-    Transaction.prototype.removeEdge = function() {
-        return this.handleAction('delete', 'edge', arguments);
-    };
+  Transaction.prototype.removeEdge = function() {
+    return this.handleAction('delete', 'edge', arguments);
+  };
 
-    Transaction.prototype.updateVertex = function() {
-        return this.handleAction('update', 'vertex', arguments);
-    };
+  Transaction.prototype.updateVertex = function() {
+    return this.handleAction('update', 'vertex', arguments);
+  };
 
-    Transaction.prototype.updateEdge = function() {
-        return this.handleAction('update', 'edge', arguments);
-    };
+  Transaction.prototype.updateEdge = function() {
+    return this.handleAction('update', 'edge', arguments);
+  };
 
-    return Transaction;
+  return Transaction;
 })();
