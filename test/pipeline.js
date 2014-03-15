28f7e456b5d42593844f465510e4387a494e7e0f
Gulthor <jbmusso+github@gmail.com>
2014-03-15 10:42:58 +0100

    Add more descriptions for pipeline tests

--- test/pipeline.js
+++ test/pipeline.js
@@ -19,125 +19,161 @@ before(function(done) {
 
 describe('Gremlin steps', function() {
   describe('Transform-based steps', function() {
-    it('should chain .V()', function() {
-      var query = gRex.gremlin().g.V();
-      query.gremlin.script.should.equal('g.V()');
+
+    describe('.V()', function () {
+      it('should handle no argument', function() {
+        var query = gRex.gremlin().g.V();
+        query.gremlin.script.should.equal('g.V()');
+      });
+
+      it('should handle key, value argument', function() {
+        var query = gRex.gremlin().g.V('name', 'marko');
+        query.gremlin.script.should.equal("g.V('name','marko')");
+      });
     });
 
-    it('should chain .V(key, value)', function() {
-      var query = gRex.gremlin().g.V('name', 'marko');
-      query.gremlin.script.should.equal("g.V('name','marko')");
+    describe('.E()', function () {
+      it('should handle no argument', function() {
+        var query = gRex.gremlin().g.E();
+        query.gremlin.script.should.equal("g.E()");
+      });
     });
 
-    it('should chain .E()', function() {
-      var query = gRex.gremlin().g.E();
-      query.gremlin.script.should.equal("g.E()");
+    describe('.v()', function () {
+      it("should handle a numerical id", function() {
+        var query = gRex.gremlin().g.v(1);
+        query.gremlin.script.should.equal("g.v(1)");
+      });
+
+      it("should handle multiple numerical ids", function() {
+        var query = gRex.gremlin().g.v(1, 4);
+        query.gremlin.script.should.equal("g.v(1,4)");
+      });
+
     });
 
-    it("should chain .v() with a numerical id", function() {
-      var query = gRex.gremlin().g.v(1);
-      query.gremlin.script.should.equal("g.v(1)");
+    describe('.id()', function () {
+      it("should be chainable", function() {
+        var query = gRex.gremlin().g.V().id();
+        query.gremlin.script.should.equal('g.V().id()');
+      });
     });
 
-    it("should chain .v() with multiple numerical ids", function() {
-      var query = gRex.gremlin().g.v(1, 4);
-      query.gremlin.script.should.equal("g.v(1,4)");
+    describe('.transform()', function () {
+      it('should handle closures', function() {
+        var query = gRex.gremlin().g.E().has('weight', T.gt, '0.5f').outV().transform('{[it.id,it.age]}');
+        query.gremlin.script.should.equal("g.E().has('weight',T.gt,0.5f).outV().transform(){[it.id,it.age]}");
+      });
     });
 
-    it("should chain .id()", function() {
-      var query = gRex.gremlin().g.V().id();
-      query.gremlin.script.should.equal('g.V().id()');
-    });
-
-    it('should chain .transform() with a closure', function() {
-      var query = gRex.gremlin().g.E().has('weight', T.gt, '0.5f').outV().transform('{[it.id,it.age]}');
-      query.gremlin.script.should.equal("g.E().has('weight',T.gt,0.5f).outV().transform(){[it.id,it.age]}");
-    });
-
+    describe('.select()', function () {
     /**
      * Select the named steps to emit after select with post-processing closures.
      * @see http://gremlindocs.com/#transform/select
      */
-    it('should handle no argument', function() {
-      var query = gRex.gremlin().g.v(1).as('x').out('knows').as('y').select();
-      query.gremlin.script.should.equal("g.v(1).as('x').out('knows').as('y').select()");
+      it('should handle no argument', function() {
+        var query = gRex.gremlin().g.v(1).as('x').out('knows').as('y').select();
+        query.gremlin.script.should.equal("g.v(1).as('x').out('knows').as('y').select()");
+      });
+
+      it('should handle an array of strings', function() {
+        var query = gRex.gremlin().g.v(1).as('x').out('knows').as('y').select(["y"]);
+        query.gremlin.script.should.equal("g.v(1).as('x').out('knows').as('y').select([\"y\"])");
+      });
+
+      it('should handle an array of strings & 1 post-processing closure', function() {
+        var query = gRex.gremlin().g.v(1).as('x').out('knows').as('y').select(["y"],"{it.name}");
+        query.gremlin.script.should.equal("g.v(1).as('x').out('knows').as('y').select([\"y\"]){it.name}");
+      });
+
+      it('should handle no argument & 2 post-processing closures', function() {
+        var query = gRex.gremlin().g.v(1).as('x').out('knows').as('y').select("{it.id}{it.name}");
+        query.gremlin.script.should.equal("g.v(1).as('x').out('knows').as('y').select(){it.id}{it.name}");
+      });
     });
 
-    it('should handle an array of strings', function() {
-      var query = gRex.gremlin().g.v(1).as('x').out('knows').as('y').select(["y"]);
-      query.gremlin.script.should.equal("g.v(1).as('x').out('knows').as('y').select([\"y\"])");
-    });
-
-    it('should handle an array of strings & 1 post-processing closure', function() {
-      var query = gRex.gremlin().g.v(1).as('x').out('knows').as('y').select(["y"],"{it.name}");
-      query.gremlin.script.should.equal("g.v(1).as('x').out('knows').as('y').select([\"y\"]){it.name}");
-    });
-
-    it('should handle no argument & 2 post-processing closures', function() {
-      var query = gRex.gremlin().g.v(1).as('x').out('knows').as('y').select("{it.id}{it.name}");
-      query.gremlin.script.should.equal("g.v(1).as('x').out('knows').as('y').select(){it.id}{it.name}");
-    });
-
-    it("should handle ordering class argument", function() {
-      var query = gRex.gremlin().g.V().both().groupCount().cap().orderMap(T.decr);
-      query.gremlin.script.should.equal("g.V().both().groupCount().cap().orderMap(T.decr)");
+    describe('.orderMap()', function () {
+      it("should handle ordering class argument", function() {
+        var query = gRex.gremlin().g.V().both().groupCount().cap().orderMap(T.decr);
+        query.gremlin.script.should.equal("g.V().both().groupCount().cap().orderMap(T.decr)");
+      });
     });
   });
 
   describe('Filter-based steps', function() {
-    it('should chain .index() as []', function() {
-      var query = gRex.gremlin().g.V().index(0).property('name');
-      query.gremlin.script.should.equal("g.V()[0].property('name')");
+    describe('[i]', function () {
+      it('should chain .index(i) as [i]', function() {
+        var query = gRex.gremlin().g.V().index(0).property('name');
+        query.gremlin.script.should.equal("g.V()[0].property('name')");
+      });
     });
 
-    it('should chain .range() as [..]', function() {
-      var query = gRex.gremlin().g.V().range('0..<2').property('name');
-      query.gremlin.script.should.equal("g.V()[0..<2].property('name')");
+    describe('[i...i]', function () {
+      it('should chain .range() as [i..j]', function() {
+        var query = gRex.gremlin().g.V().range('0..<2').property('name');
+        query.gremlin.script.should.equal("g.V()[0..<2].property('name')");
+      });
     });
 
-    it('should chain .and() with 2 conditions', function() {
-      var gremlin = gRex.gremlin();
-      var query = gremlin.g.V().and(gremlin._().both("knows"), gremlin._().both("created"));
-      query.gremlin.script.should.equal("g.V().and(_().both('knows'),_().both('created'))");
+    describe('.and()', function () {
+      it('should chain .and() with two conditions', function() {
+        var gremlin = gRex.gremlin();
+        var query = gremlin.g.V().and(gremlin._().both("knows"), gremlin._().both("created"));
+
+        query.gremlin.script.should.equal("g.V().and(_().both('knows'),_().both('created'))");
+      });
     });
 
-    it('should chain .or() with 2 conditions', function() {
-      var gremlin = gRex.gremlin();
-      var query = gremlin.g.v(1).outE().or(gremlin._().has('id', T.eq, 9), gremlin._().has('weight', T.lt, '0.6f'));
-      query.gremlin.script.should.equal("g.v(1).outE().or(_().has('id',T.eq,9),_().has('weight',T.lt,0.6f))");
+    describe('.or()', function () {
+      it('should chain .or() with two conditions', function() {
+        var gremlin = gRex.gremlin();
+        var query = gremlin.g.v(1).outE().or(gremlin._().has('id', T.eq, 9), gremlin._().has('weight', T.lt, '0.6f'));
+
+        query.gremlin.script.should.equal("g.v(1).outE().or(_().has('id',T.eq,9),_().has('weight',T.lt,0.6f))");
+      });
     });
 
-    it('should chain .retain([])', function() {
-      var gremlin = gRex.gremlin();
+    describe('.retain()', function () {
+      it('should handle an array of pipelines', function() {
+        var gremlin = gRex.gremlin();
+        var query = gremlin.g.V().retain([gremlin.g.v(1), gremlin.g.v(2), gremlin.g.v(3)]);
 
-      var query = gremlin.g.V().retain([gremlin.g.v(1), gremlin.g.v(2), gremlin.g.v(3)]);
-
-      query.gremlin.script.should.equal("g.V().retain([g.v(1),g.v(2),g.v(3),])");
+        query.gremlin.script.should.equal("g.V().retain([g.v(1),g.v(2),g.v(3),])");
+      });
     });
 
-    it('should chain .except()', function() {
-      var query = gRex.gremlin().g.V().has('age',T.lt,30).as('x').out('created').in('created').except('x');
-      query.gremlin.script.should.equal("g.V().has('age',T.lt,30).as('x').out('created').in('created').except('x')");
+    describe('.except()', function () {
+      it('should chain .except()', function() {
+        var query = gRex.gremlin().g.V().has('age',T.lt,30).as('x').out('created').in('created').except('x');
+
+        query.gremlin.script.should.equal("g.V().has('age',T.lt,30).as('x').out('created').in('created').except('x')");
+      });
     });
   });
 
   describe('SideEffect-based steps', function() {
-    it("should chain .gather()", function() {
-      var query = gRex.gremlin().g.v(1).out().gather("{it.size()}");
-      query.gremlin.script.should.equal("g.v(1).out().gather(){it.size()}");
+    describe('.gather()', function () {
+      it("should handle a closure", function() {
+        var query = gRex.gremlin().g.v(1).out().gather("{it.size()}");
+        query.gremlin.script.should.equal("g.v(1).out().gather(){it.size()}");
+      });
     });
   });
 
   describe('Branch-based steps', function() {
-    it("should chain .ifThenElse()", function() {
-      var query = gRex.gremlin().g.v(1).out().ifThenElse("{it.name=='josh'}{it.age}{it.name}");
-      query.gremlin.script.should.equal("g.v(1).out().ifThenElse(){it.name=='josh'}{it.age}{it.name}");
+    describe('.copySplit()', function () {
+      it("should chain .copySplit()", function() {
+        var gremlin = gRex.gremlin();
+        var query = gremlin.g.v(1).out('knows').copySplit(gremlin._().out('created').property('name'), gremlin._().property('age')).fairMerge();
+        query.gremlin.script.should.equal("g.v(1).out('knows').copySplit(_().out('created').property('name'),_().property('age')).fairMerge()");
+      });
     });
 
-    it("should chain .copySplit()", function() {
-      var gremlin = gRex.gremlin();
-      var query = gremlin.g.v(1).out('knows').copySplit(gremlin._().out('created').property('name'), gremlin._().property('age')).fairMerge();
-      query.gremlin.script.should.equal("g.v(1).out('knows').copySplit(_().out('created').property('name'),_().property('age')).fairMerge()");
+    describe('.ifThenElse()', function () {
+      it("should chain .ifThenElse()", function() {
+        var query = gRex.gremlin().g.v(1).out().ifThenElse("{it.name=='josh'}{it.age}{it.name}");
+        query.gremlin.script.should.equal("g.v(1).out().ifThenElse(){it.name=='josh'}{it.age}{it.name}");
+      });
     });
   });
 });
