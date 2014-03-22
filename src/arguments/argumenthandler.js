a16481d57652aa5c8f6cecb49e575337825e6909
Gulthor <jbmusso+github@gmail.com>
2014-03-22 11:46:04 +0100

    Extract buildArguments() method from ArgumentHandler and move it to ArgumentList class

--- src/arguments/argumenthandler.js
+++ src/arguments/argumenthandler.js
@@ -1,8 +1,6 @@
 var _ = require("lodash");
 
-var Argument = require('./argument');
-var ClosureArgument = require('./closure');
-var ObjectArgument = require('./object');
+var ArgumentList = require('./argumentlist');
 
 module.exports = (function () {
   function ArgumentHandler(options) {
@@ -10,26 +8,10 @@ module.exports = (function () {
   }
 
   ArgumentHandler.prototype.buildString = function(args, retainArray) {
-    var argList = [];
-    var append = [];
+    var argList = new ArgumentList(args, this.options);
+    argList.buildArguments(retainArray);
 
-    _.each(args, function(arg) {
-      if (this.isClosure(arg)) {
-        arg = new ClosureArgument(arg);
-        append.push(arg.toString());
-      } else if(retainArray && _.isArray(arg)) {
-        arg = new Argument(arg, this.options);
-        argList.push("[" + arg.parse() + "]");
-      } else if (_.isObject(arg)) {
-        arg = new ObjectArgument(arg);
-        argList.push(arg.toString());
-      } else {
-        arg = new Argument(arg, this.options);
-        argList.push(arg.parse());
-      }
-    }, this);
-
-    return '(' + argList.join(',') + ')' + append.join(',');
+    return argList.toString();
   };
 
   ArgumentHandler.prototype.handleArray = function(args) {
@@ -42,11 +24,6 @@ module.exports = (function () {
     return '(' + argumentList.join(',') + ')';
   };
 
-  ArgumentHandler.prototype.isClosure = function(val) {
-    var closureRegex = /^\{.*\}$/;
-
-    return _.isString(val) && closureRegex.test(val);
-  };
 
   return ArgumentHandler;
 
--- src/arguments/argumentlist.js
+++ src/arguments/argumentlist.js
@@ -1,10 +1,45 @@
+var _ = require('lodash');
+
+var Argument = require('./argument');
+var ClosureArgument = require('./closure');
+var ObjectArgument = require('./object');
+
+
 module.exports = (function () {
-  function ArgumentList() {
-    this.args = [];
+  function ArgumentList(rawArgs, options) {
+    this.rawArgs = rawArgs;
+    this.options = options;
+
+    this.parenthesizedArguments = [];
+    this.appendedArguments = [];
   }
 
   ArgumentList.prototype.toString = function() {
-    return this.args.join(',');
+    return '(' + this.parenthesizedArguments.join(',') + ')' + this.appendedArguments.join(',');
+  };
+
+  ArgumentList.prototype.buildArguments = function(retainArray) {
+    _.each(this.rawArgs, function(arg) {
+      if (this.isClosure(arg)) {
+        arg = new ClosureArgument(arg);
+        this.appendedArguments.push(arg.toString());
+      } else if(retainArray && _.isArray(arg)) {
+        arg = new Argument(arg, this.options);
+        this.parenthesizedArguments.push("[" + arg.parse() + "]");
+      } else if (_.isObject(arg)) {
+        arg = new ObjectArgument(arg);
+        this.parenthesizedArguments.push(arg.toString());
+      } else {
+        arg = new Argument(arg, this.options);
+        this.parenthesizedArguments.push(arg.parse());
+      }
+    }, this);
+  };
+
+  ArgumentList.prototype.isClosure = function(val) {
+    var closureRegex = /^\{.*\}$/;
+
+    return _.isString(val) && closureRegex.test(val);
   };
 
   return ArgumentList;
