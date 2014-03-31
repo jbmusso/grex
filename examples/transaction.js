e4600bf2374fd007b385c8da5099b446668a5e8a
Salim Alami <azerty_161@hotmail.com>
2014-03-31 16:18:23 +0200

    Update transaction example to latest version

--- examples/transaction.js
+++ examples/transaction.js
@@ -7,23 +7,24 @@ var settings = {
   'idRegex': /^[0-9]+:[0-9]+$/
 };
 
-Grex.connect(function(err, client) {
+Grex.connect(settings, function(err, client) {
   var gremlin = client.gremlin();
-
+  var g = gremlin.g;
+  
   // The following will add 4 vertices and 2 edges in a transaction
 
-  var v1 = gremlin.g.addVertex({ name: 'Foo', age: 20 }, 'v1');
+  var v1 = g.addVertex({ name: 'Foo', age: 20 }, 'v1');
   v1.addProperty('name2', 'testa');
   v1.setProperty('name2', 'updated');
 
-  var v2 = gremlin.g.addVertex({ name:'Bar', age: 30 }, 'v2');
-  gremlin.g.addEdge(v1, v2, 'knows', { met: "Somewhere", weight: 1.2 });
+  var v2 = g.addVertex({ name:'Bar', age: 30 }, 'v2');
+  g.addEdge(v1, v2, 'knows', { met: "Somewhere", weight: 1.2 });
 
-  var v3 = trxn.addVertex({ name: 'Alice' });
-  var v4 = trxn.addVertex({ name: 'Bob' });
-  gremlin.g.addEdge(v4, v3, 'likes', { key: "value" });
+  var v3 = g.addVertex({ name: 'Alice' }, 'v3');
+  var v4 = g.addVertex({ name: 'Bob' }, 'v4');
+  g.addEdge(v4, v3, 'likes', { key: "value" });
 
-  trxn.commit(function(err, response) {
+  gremlin.exec(function(err, response) {
     if (err) {
       console.log(err);
     } else {
