3e1c25f4cb4e3c2a6f2e71c3943a70e7779b834c
Gulthor <jbmusso+github@gmail.com>
2014-05-20 21:11:26 +0200

    Add connect() description in RexsterClient tests

--- test/client.js
+++ test/client.js
@@ -12,47 +12,49 @@ var defaultOptions = {
 };
 
 
-describe('RexsterClient', function() {
-  describe('when passing no parameters', function() {
-    it('should use default options', function(done) {
-      grex.connect(function(err, client) {
-        should.not.exist(err);
-        client.options.should.eql(defaultOptions);
-        done();
-      });
-    });
-  });
-
-  describe('when passing custom options', function() {
-    var options = {
-      'host': 'localhost',
-      'port': 8182,
-      'graph': 'gratefulgraph'
-    };
-
-    before(function(done) {
-      grex.connect(options, function(err, client) {
-        done();
+describe.only('RexsterClient', function() {
+  describe('connect()', function() {
+    describe('when passing no parameters', function() {
+      it('should use default options', function(done) {
+        grex.connect(function(err, client) {
+          should.not.exist(err);
+          client.options.should.eql(defaultOptions);
+          done();
+        });
       });
     });
 
-    it('should use this new options', function(done) {
-      grex.options.graph.should.equal(options.graph);
-      done();
+    describe('when passing custom options', function() {
+      var options = {
+        'host': 'localhost',
+        'port': 8182,
+        'graph': 'gratefulgraph'
+      };
+
+      before(function(done) {
+        grex.connect(options, function(err, client) {
+          done();
+        });
+      });
+
+      it('should use this new options', function(done) {
+        grex.options.graph.should.equal(options.graph);
+        done();
+      });
     });
-  });
 
-  describe('when instantiating a client with custom options', function() {
-    var options = {
-      'host': 'localhost',
-      'port': 8182,
-      'graph': 'gratefulgraph'
-    };
+    describe('when instantiating a client with custom options', function() {
+      var options = {
+        'host': 'localhost',
+        'port': 8182,
+        'graph': 'gratefulgraph'
+      };
 
-    it('should use the right options', function(done) {
-      var client = new RexsterClient(options);
-      client.options.graph.should.equal(options.graph);
-      done();
+      it('should use the right options', function(done) {
+        var client = new RexsterClient(options);
+        client.options.graph.should.equal(options.graph);
+        done();
+      });
     });
   });
 });
