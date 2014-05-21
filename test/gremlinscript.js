c33603319cd5464de063312dec85a8a84a0d5203
Gulthor <jbmusso+github@gmail.com>
2014-05-21 21:33:43 +0200

    Add descriptions to GremlinScript tests

--- test/gremlinscript.js
+++ test/gremlinscript.js
@@ -1,16 +1,19 @@
 var GremlinScript = require('../src/gremlinscript');
 
 describe('GremlinScript', function() {
-  it('should append script', function() {
-    var gremlin = new GremlinScript();
-    gremlin.append('foo');
-    gremlin.script.should.equal('foo');
-
+  describe('append()', function() {
+    it('should append script', function() {
+      var gremlin = new GremlinScript();
+      gremlin.append('foo');
+      gremlin.script.should.equal('foo');
+    });
   });
 
-  it('should append a new line', function() {
-    var gremlin = new GremlinScript();
-    gremlin.line('foo');
-    gremlin.script.should.equal('\nfoo');
+  describe('line()', function() {
+    it('should append a new line', function() {
+      var gremlin = new GremlinScript();
+      gremlin.line('foo');
+      gremlin.script.should.equal('\nfoo');
+    });
   });
 });
