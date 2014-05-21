c04a1aa6aa556dd4c850bdbb0283cdea468d9d80
Gulthor <jbmusso+github@gmail.com>
2014-05-21 20:57:46 +0200

    Add more tests for RexsterClient class

--- test/client.js
+++ test/client.js
@@ -3,6 +3,7 @@ var should = require('should');
 var grex = require('../index.js');
 var _    = require("lodash");
 var RexsterClient = require('../src/rexsterclient.js');
+var GremlinScript = require('../src/gremlinscript.js');
 
 
 var defaultOptions = {
@@ -12,7 +13,7 @@ var defaultOptions = {
 };
 
 
-describe.only('RexsterClient', function() {
+describe('RexsterClient', function() {
   describe('connect()', function() {
     describe('when passing no parameters', function() {
       it('should use default options', function(done) {
@@ -54,6 +55,48 @@ describe.only('RexsterClient', function() {
         var client = new RexsterClient(options);
         client.options.graph.should.equal(options.graph);
         done();
+      });
+    });
+  });
+
+  describe('gremlin()', function() {
+    var client;
+
+    before(function() {
+      client = new RexsterClient();
+    });
+
+    describe('with no parameters', function() {
+      var gremlin;
+
+      before(function() {
+        gremlin = client.gremlin();
+      });
+
+      it('should return an instance of GremlinScript', function() {
+        gremlin.should.be.an.instanceof(GremlinScript);
+      });
+
+      it('should be an empty script', function() {
+        gremlin.script.should.equal('');
+        gremlin.params.should.eql({});
+      });
+    });
+
+    describe('with a Gremlin object wrapper as a parameter', function() {
+      var gremlin;
+      var g = grex.g;
+
+      before(function() {
+        gremlin = client.gremlin(g.v(1));
+      });
+
+      it('should return an instance of GremlinScript', function() {
+        gremlin.should.be.an.instanceof(GremlinScript);
+      });
+
+      it('should have a non empty script', function() {
+        gremlin.script.should.equal('g.v(1)');
       });
     });
   });
