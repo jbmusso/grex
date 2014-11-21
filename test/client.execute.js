81567936fdb2bd41a5ec95f8736d618ff6f41b03
gulthor <jbmusso+github@gmail.com>
2014-11-21 23:25:44 +0100

    Remove client.execute test descriptions

--- test/client.execute.js
+++ test/client.execute.js
@@ -15,81 +15,58 @@ describe('client', function() {
         should.exist(response.results);
       });
     });
-  });
 
-  describe('.execute()', function() {
-    describe('when port is incorrect', function() {
-      it('should return an error', function(done) {
-        var client = grex.createClient({
-          host: 'localhost',
-          port: 123456,
-          graph: 'tinkergraph'
-        });
-
-        client.execute(gremlin(g.v(1)), function(err, results) {
-          should.exist(err);
-          should.not.exist(results);
-          done();
-        });
-      });
-    });
-
-    describe('when host is incorrect', function() {
-      it('should return an error', function(done) {
-        var options = {
-          'host': 'local-host',
-          'port': 8182,
-          'graph': 'tinkergraph'
-        };
-        var client = grex.createClient(options);
-
-        client.execute(gremlin(g.v(1)), function(err, results) {
-          should.exist(err);
-          should.not.exist(results);
-          done();
-        });
-      });
-    });
-
-    describe('when graph name is incorrect', function() {
-      it('should return an error', function(done) {
-        var options = {
-          'host': 'localhost',
-          'port': 8182,
-          'graph': 'tinker-graph'
-        };
-
-        var client = grex.createClient(options);
-
-        client.execute(gremlin(g.v(1)), function(err, results) {
-          should.exist(err);
-          should.not.exist(results);
-          done();
-        });
-      });
-    });
-
-    describe('when passing an ObjectWrapper', function() {
+    it('should automatically instantiate a GremlinScript and execute it', function() {
       var client = grex.createClient();
-
-      it('should lazily instantiate a GremlinScript and execute it', function() {
-        client.execute(g.v(1), function(err, results) {
-          should.not.exist(err);
-          should.exist(results);
-        });
+      client.execute(g.v(1), function(err, results) {
+        should.not.exist(err);
+        should.exist(results);
       });
     });
 
-    describe('when passing a query', function() {
-      var client = grex.createClient();
+    it('should return an error when port is incorrect', function(done) {
+      var client = grex.createClient({
+        host: 'localhost',
+        port: 123456,
+        graph: 'tinkergraph'
+      });
 
-      it('should execute it', function() {
-        var query = gremlin(g.v(1));
+      client.execute(gremlin(g.v(1)), function(err, results) {
+        should.exist(err);
+        should.not.exist(results);
+        done();
+      });
+    });
 
-        client.execute(query, function(err, results) {
-          should.not.exist(err);
-          should.exist(results);
-        });
+
+    it('should return an error when host is incorrect', function(done) {
+      var options = {
+        'host': 'local-host',
+        'port': 8182,
+        'graph': 'tinkergraph'
+      };
+      var client = grex.createClient(options);
+
+      client.execute(gremlin(g.v(1)), function(err, results) {
+        should.exist(err);
+        should.not.exist(results);
+        done();
+      });
+    });
+
+    it('should return an error when graph name is incorrect', function(done) {
+      var options = {
+        'host': 'localhost',
+        'port': 8182,
+        'graph': 'tinker-graph'
+      };
+
+      var client = grex.createClient(options);
+
+      client.execute(gremlin(g.v(1)), function(err, results) {
+        should.exist(err);
+        should.not.exist(results);
+        done();
       });
     });
   });
